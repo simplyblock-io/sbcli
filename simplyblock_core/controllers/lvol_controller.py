c1dabed39f8cbe82cb69fcf4638d40bd30bdef5c
ashwin-simplyblock <ash@simplyblock.io>
2025-03-07 04:05:31 -0600

    add initial tiering params on lvol creation (storage tiering param updates still a WIP)

--- simplyblock_core/controllers/lvol_controller.py
+++ simplyblock_core/controllers/lvol_controller.py
@@ -464,12 +464,19 @@ def add_lvol_ha(name, size, host_id_or_name, ha_type, pool_id_or_name, use_comp,
         "params": {
             "name": lvol.lvol_bdev,
             "size_in_mib": int(lvol.size / (1000 * 1000)),
-            "lvs_name": lvol.lvs_name
+            "lvs_name": lvol.lvs_name,
         }
     }
 
     if lvol.lvol_priority_class:
         lvol_dict["params"]["lvol_priority_class"] = lvol.lvol_priority_class
+    
+    lvol_dict['params']['is_tiered'] = is_tiered
+    lvol_dict['params']['force_fetch'] = force_fetch
+    lvol_dict['params']['sync_fetch'] = sync_fetch
+    lvol_dict['params']['pure_flush_or_evict'] = pure_flush_or_evict
+    lvol_dict['params']['not_evict_blob_md'] = not_evict_blob_md
+    
     lvol.bdev_stack = [lvol_dict]
 
     if use_crypto:
