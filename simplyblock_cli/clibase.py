6e0ec739e85dbbb692970add8e24d9b44c633fbf
Max Schettler <max@simplyblock.io>
2025-03-26 11:00:36 +0100

    Add CLI size type

--- simplyblock_cli/clibase.py
+++ simplyblock_cli/clibase.py
@@ -16,6 +16,22 @@ from simplyblock_core.controllers import caching_node_controller, health_control
 from simplyblock_core.models.pool import Pool
 
 
+def size_type(min=None, max=None):
+    def f(arg):
+        size = utils.parse_size(arg)
+
+        if size == -1:
+            raise argparse.ArgumentTypeError(f"Invalid size '{arg}' passed")
+        elif min is not None and size < min:
+            raise argparse.ArgumentTypeError(f"Size must be larger than {utils.humanbytes(min)}")
+        elif max is not None and size > max:
+            raise argparse.ArgumentTypeError(f"Size must be smaller than {utils.humanbytes(max)}")
+
+        return size
+
+    return f
+
+
 def regex_type(regex):
     def f(arg):
         if (match := re.match(regex, arg)) is not None:
@@ -363,8 +379,8 @@ class CLIWrapperBase:
 
     def volume__add(self, sub_command, args):
         name = args.name
-        size = utils.parse_size(args.size)
-        max_size = utils.parse_size(args.max_size)
+        size = args.size
+        max_size = args.max_size
         host_id = args.host_id
         ha_type = args.ha_type
         pool = args.pool
@@ -418,7 +434,7 @@ class CLIWrapperBase:
 
     def volume__resize(self, sub_command, args):
         volume_id = args.volume_id
-        size = utils.parse_size(args.size)
+        size = args.size
         return lvol_controller.resize_lvol(volume_id, size)
 
     def volume__create_snapshot(self, sub_command, args):
@@ -428,9 +444,7 @@ class CLIWrapperBase:
         return snapshot_id if not error else error
 
     def volume__clone(self, sub_command, args):
-        new_size = 0
-        if args.resize:
-            new_size = utils.parse_size(args.resize)
+        new_size = args.resize
 
         clone_id, error = snapshot_controller.clone(args.snapshot_id, args.clone_name, new_size)
         return clone_id if not error else error
@@ -483,8 +497,8 @@ class CLIWrapperBase:
             has_secret = False
         return pool_controller.add_pool(
             args.name,
-            utils.parse_size(args.pool_max),
-            utils.parse_size(args.lvol_max),
+            args.pool_max,
+            args.lvol_max,
             args.max_rw_iops,
             args.max_rw_mbytes,
             args.max_r_mbytes,
@@ -494,12 +508,9 @@ class CLIWrapperBase:
         )
 
     def storage_pool__set(self, sub_command, args):
-        pool_max = None
-        lvol_max = None
-        if args.pool_max:
-            pool_max = utils.parse_size(args.pool_max)
-        if args.lvol_max:
-            lvol_max = utils.parse_size(args.lvol_max)
+        pool_max = args.pool_max if args.pool_max else None
+        lvol_max = args.lvol_max if args.lvol_max else None
+
         return pool_controller.set_pool(
             args.pool_id,
             pool_max,
@@ -547,9 +558,7 @@ class CLIWrapperBase:
         return snapshot_controller.delete(args.snapshot_id, args.force)
 
     def snapshot__clone(self, sub_command, args):
-        new_size = 0
-        if args.resize:
-            new_size = utils.parse_size(args.resize)
+        new_size = args.resize
 
         success, details = snapshot_controller.clone(args.snapshot_id, args.lvol_name, new_size)
         return details
@@ -566,12 +575,7 @@ class CLIWrapperBase:
         namespace = args.namespace
         multipathing = args.multipathing == "on"
         spdk_cpu_mask = args.spdk_cpu_mask if 'spdk_cpu_mask' in args else None
-
-        spdk_mem = None
-        if args.spdk_mem:
-            spdk_mem = utils.parse_size(args.spdk_mem)
-            if spdk_mem < utils.parse_size('1GiB'):
-                return f"SPDK memory:{args.spdk_mem} must be larger than 1GiB"
+        spdk_mem = args.spdk_mem if 'spdk_mem' in args else None
 
         return caching_node_controller.add_node(
             cluster_id, node_ip, ifname, data_nics, spdk_cpu_mask, spdk_mem, spdk_image, namespace, multipathing)
@@ -684,11 +688,11 @@ class CLIWrapperBase:
         secondary_nodes = args.secondary_nodes
         
         lvol_name = args.lvol_name
-        lvol_size = utils.parse_size(args.lvol_size)
-        max_size = utils.parse_size(args.max_size)
+        lvol_size = args.lvol_size
+        max_size = args.max_size
         lvol_ha_type = args.lvol_ha_type
         pool_name = args.pool_name
-        pool_max = utils.parse_size(args.pool_max)
+        pool_max = args.pool_max
         host_id = args.host_id
         comp = None
         crypto = args.encrypt
