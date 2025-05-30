1122cf9550139548b245fabdee5c5dc9a0671fc3
Max Schettler <max@simplyblock.io>
2025-05-30 15:13:36 +0200

    Add API v2

--- simplyblock_web/api/v1/__init__.py
+++ simplyblock_web/api/v1/__init__.py
@@ -1,5 +1,7 @@
 from flask import Blueprint
 
+from simplyblock_web.auth_middleware import token_required
+
 from . import cluster
 from . import mgmt_node
 from . import device
@@ -8,12 +10,11 @@ from . import snapshot
 from . import storage_node
 from . import pool
 from . import caching_node
-from . import deployer
 from . import swagger_ui
 from . import metrics
 
 
-api = Blueprint('API v1', __name__)
+api = Blueprint('API v1', __name__, url_prefix='v1')
 api.register_blueprint(cluster.bp)
 api.register_blueprint(mgmt_node.bp)
 api.register_blueprint(device.bp)
@@ -22,6 +23,11 @@ api.register_blueprint(snapshot.bp)
 api.register_blueprint(storage_node.bp)
 api.register_blueprint(pool.bp)
 api.register_blueprint(caching_node.bp)
-api.register_blueprint(deployer.bp)
 api.register_blueprint(swagger_ui.bp, url_prefix=swagger_ui.SWAGGER_URL)
 api.register_blueprint(metrics.bp)
+
+
+@api.before_request
+@token_required
+def before_request():
+    pass
